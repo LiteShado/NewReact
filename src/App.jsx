ee67dad5667cc0d9e5b03d74101c11014ed299cf
Nat Richmond <natrichmond@Nats-MBP.lan>
2025-04-09 16:43:03 -0400

    hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,38 @@
-import { useState } from 'react'
-import './App.css'
-import Todo from './components/Todo.jsx'
-import Heading from './components/Heading.jsx'
-import Popups from './components/Popups.jsx'
+import { useState } from "react";
+import "./App.css";
+import Todo from "./components/Todo.jsx";
+import Heading from "./components/Heading.jsx";
+import Popups from "./components/Popups.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [popupOpen, setPopupOpen] = useState(false)
 
   return (
     <>
+      <Heading title="Nat's List" />
+      <div>
+        <input
+          type="text"
+          onChange={(event) => {
+            console.log(event.target.value);
+          }}
+        />
+        <button>Add to do</button>
+      </div>
+      <Todo task="learn react" />
+      <Todo task="land a junior job" />
+      <Todo task="earn $200,000+" />
 
-    <Heading title="Nat's List" />
-    <div>
-      <input type="text" onChange={(event) => {
-        console.log(event.target.value);
-      }} />
-      <button>Add to do</button>
-    </div>
-    <Todo task="learn react"
-     />
-    <Todo task="land a junior job"
-    />
-    <Todo task="earn $200,000+"
-    />
-  
-    <Popups greeting="Are you sure?" description = "ABSOLUTELY" />
-    
+      {false && (
+        <Popups
+          greeting="Are you sure?"
+          description="ABSOLUTELY"
+          cancel="cancel"
+        />
+      )}
     </>
-   
-  )
+  );
 }
 
 export default App;
