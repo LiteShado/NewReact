d62b6715d68b9e7b79c48ccaf1605d649eb4d6d8
Nat Richmond <natrichmond@Nats-MacBook-Pro.local>
2025-04-11 18:18:18 -0400

    routes specified

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import { useState, useEffect } from "react";
 import "./App.css";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import About from "./pages/About";
 
 function App() {
   
 
   return (
     <>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
       
     </>
   );
