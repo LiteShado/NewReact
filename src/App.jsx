80be0204a2a422136e85d0ef34429de0d37597c4
Nat Richmond <natrichmond@Nats-MacBook-Pro.local>
2025-04-11 18:08:37 -0400

    app jsx cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,13 @@
 import { useState, useEffect } from "react";
 import "./App.css";
-import Todo from "./components/Todo.jsx";
-import Heading from "./components/Heading.jsx";
-import Popups from "./components/Popups.jsx";
-import Counter from "./components/Counter.jsx";
+
 
 function App() {
-  const [popupOpen, setPopupOpen] = useState(false);
-
-  function togglePopup() {
-    setPopupOpen(true);
-    console.log("toggled");
-  }
-
-  function toggleCancel() {
-    setPopupOpen(false);
-  }
-
-  useEffect(() => {
-    console.log("useffect");
-  }, []) 
-  // function that I want to run is the first arguement, which is a callback function. dependency array is the second argument
+  
 
   return (
     <>
-      <Counter />
-      <Heading title="Nat's List" />
-      <div>
-        <input
-          type="text"
-          onChange={(event) => {
-            console.log(event.target.value);
-          }}
-        />
-        <button
-          onClick={() => {
-            setPopupOpen(true);
-          }}
-        >
-          Add to do
-        </button>
-      </div>
-      <Todo task="learn react" togglePopup={togglePopup} />
-      <Todo task="land a junior job" togglePopup={togglePopup} />
-      <Todo task="earn $200,000+" togglePopup={togglePopup} />
-
-      {popupOpen && (
-        <Popups
-          toggleCancel={toggleCancel}
-          greeting="Are you sure?"
-          description="ABSOLUTELY"
-          cancel="cancel"
-        />
-      )}
+      
     </>
   );
 }
