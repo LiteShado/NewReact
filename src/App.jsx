5240117962777875b504670302d968978e8d9cad
Nat Richmond <natrichmond@Nats-MacBook-Pro.local>
2025-04-11 21:29:39 -0400

    nav bar added

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,25 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Contact from "./pages/Contact";
 import About from "./pages/About";
+import Posts from "./pages/Posts";
 
 function App() {
-  
-
   return (
     <>
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </Router>
-      
+      <Router>
+        <nav>
+          <a href="/">Home</a>
+          <a href="/about">About</a>
+          <a href="/posts">Posts</a>
+          <a href="/contact">Contact</a>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/posts" element={<Posts />} />
+        </Routes>
+      </Router>
     </>
   );
 }
---
+++ src/pages/Posts.jsx
@@ -0,0 +1,11 @@
+function Posts() {
+  return (
+    <>
+      
+        <h1>Posts</h1>
+    
+    </>
+  );
+}
+
+export default Posts;
